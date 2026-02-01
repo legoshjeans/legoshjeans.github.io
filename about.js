ef5baded8d8f6b7f9a9813e71db052ba136825b3
legoshjeans <cstrendfidi@gmail.com>
2026-02-02 04:22:44 +0700

    Update about.js
    Signed-off-by: legoshjeans <cstrendfidi@gmail.com>

--- about.js
+++ about.js
@@ -5,5 +5,19 @@ document.getElementById("about").innerHTML = `
 <p>Selain mengutamakan bahan, semua aksesoriesnya juga terkesan sangat premium. Senada dengan brandnya "Legosh", yang terbukti membawa karirnya membumbung. Tidak seperti produk celana jeans pada umumnya, legosh didesain dengan kancing anti karat serta zipper auto-lock banget. Bahkan kualitasnya sudah sangat teruji dan punya sertifikat maupun penerbitan standar SNI.</p>
 <h2>Tentang Kami</h2>
 <p>Legosh jeans adalah situs berbagi katalog produk celana pria legosh. Selain itu, anda juga dapat menemukan produk legosh lainnya seperti jaket pria dan wanita. Semuanya disajikan spesial untuk mempermudah siapapun menemukan brand legosh asli original yang mana, siapapun dapat melihat detail harga, deskripsi produk, toko dan alamat penjual setelah mengakses tombol detail pada setiap katalog. Segera, miliki fashion terbaik kamu hari ini dan simpan halaman ini sebagai rujukan karena akan selalu di update setiap harinya.</p>
+<h2>Mengapa Memilih Celana Legosh</h2>
+Walaupun pemasarannya belum begitu populer, legosh jeans sudah terbukti menembus pasar indonesia dan berhasil meraih penjualan hingga jutaan produk. Mungkin dimasa mendatang, jumlah peminatnya akan semakin tinggi seiring orang mengenal brand legosh. Semua ini tidak lepas dari beberapa kualitas berikut:
+<ol><li>
+<h3>Soft Jeans Premium</h3>kain yang dipakai sebagai material ini dari soft jeans grade A, memungkinkan teksturnya terasa lembut. Bahkan elastisitasnya sangat tinggi. Disatu sisi, kenyaman bagi pemakai juga terus ditingkatkan.
+</li><li><h3>Bahan Canvas Premium</h3>
+Tak hanya sekedar nama, tapi celana legosh ini menawarkan bahan canvas premium yang dipastikan tebal, halus tetapi tidak kaku. Hal ini mendorong mayoritas fashionable menjadikannya sebagai jeans casual.
+</li><li><h3>Punya Karakteristik Unik</h3>
+Tidak seperti celana jeans yang beredar digrosir atau pasaran, tetapi legosh mengutamakan bahan tebal dan kuat sehingga tahan lama dalam hal pemakaian. Selain itu, legosh juga menerapkan material dari strect sebagai bahan dasar kanvasnya.
+</li><li><h3>Tersedia Opsi Warna</h3>
+Ketika seseorang bosan dengan satu warna, justru legosh memanfaatkan moment ini untuk menciptakan opsi minat bagi konsumen. Hal ini merujuk pada warna yang mencakup abu-abu, coklat, hitam, dan biru dongker. Semuanya dibuat dengan material berkualitas.
+</li><li><h3>Harga Menyesuaikan Ekonomi Indonesia</h3>
+Meski celana legosh adalah brand terbaik dalam kategori jeans casual original, legosh selalu memberi kesempatan bagi siapapun agar bisa memiliki satu atau beberapa jeans. Dengan pola penerapan harga kompetitif, menjadikan legosh sebagai satu-satunya opsi celana jeans pria yang punya harga sangat terjangkau.
+</li></ol>
+Beberapa alasan diatas merupakan sebagian kecil mengapa orang Indonesia wajib memilih celana panjang pria legosh.
 </section>
 `;
