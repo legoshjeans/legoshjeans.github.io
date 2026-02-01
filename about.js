c544f8944997911d44758eceaa0c32fe0555986b
legoshjeans <cstrendfidi@gmail.com>
2026-02-01 15:19:50 +0700

    Update about.js
    Signed-off-by: legoshjeans <cstrendfidi@gmail.com>

--- about.js
+++ about.js
@@ -1,6 +1,6 @@
 document.getElementById("about").innerHTML = `
 <section>
   <h2>Tentang Kami</h2>
-  <p>Kami menjual celana Legosh original dengan bahan adem dan nyaman.</p>
+  <p>Legosh jeans adalah situs berbagi katalog produk celana pria legosh. Selain itu, anda juga dapat menemukan produk legosh lainnya seperti jaket pria dan wanita. Semuanya disajikan spesial untuk mempermudah siapapun menemukan brand legosh asli original yang mana, siapapun dapat melihat detail harga, deskripsi produk, toko dan alamat penjual setelah mengakses tombol detail pada setiap katalog. Segera, miliki fashion terbaik kamu hari ini dan simpan halaman ini sebagai rujukan karena akan selalu di update setiap harinya.</p>
 </section>
 `;
