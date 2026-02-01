a8b492511e985f799b343c028ca8fbb80ff1ed72
legoshjeans <cstrendfidi@gmail.com>
2026-02-01 17:56:50 +0700

    Update about.js
    Signed-off-by: legoshjeans <cstrendfidi@gmail.com>

--- about.js
+++ about.js
@@ -1,6 +1,9 @@
 document.getElementById("about").innerHTML = `
 <section>
-  <h2>Tentang Kami</h2>
-  <p>Legosh jeans adalah situs berbagi katalog produk celana pria legosh. Selain itu, anda juga dapat menemukan produk legosh lainnya seperti jaket pria dan wanita. Semuanya disajikan spesial untuk mempermudah siapapun menemukan brand legosh asli original yang mana, siapapun dapat melihat detail harga, deskripsi produk, toko dan alamat penjual setelah mengakses tombol detail pada setiap katalog. Segera, miliki fashion terbaik kamu hari ini dan simpan halaman ini sebagai rujukan karena akan selalu di update setiap harinya.</p>
+<h2>Tentang Legosh</h2>
+<p>Brand legosh memang terkenal populer dikalangan masyarakat luas. hal inilah yang mengundang saya untuk membuat halaman ini sebagai bahan rujukan bersama. Popularitas legosh sendiri juga tidak lepas dari visinya yang selalu mengutamakan produk, seperti menerapkan bahan denim/canvas grade A tebal, lembut serta tahan lama. Jelas ini alasan mendasar orang-orang memilih legosh sampai hari ini.</p>
+<p>Selain mengutamakan bahan, semua aksesoriesnya juga terkesan sangat premium. Senada dengan brandnya "Legosh", yang terbukti membawa karirnya membumbung. Tidak seperti produk celana jeans pada umumnya, legosh didesain dengan kancing anti karat serta zipper auto-lock banget. Bahkan kualitasnya sudah sangat teruji dan punya sertifikat maupun penerbitan standar SNI.</p>
+<h2>Tentang Kami</h2>
+<p>Legosh jeans adalah situs berbagi katalog produk celana pria legosh. Selain itu, anda juga dapat menemukan produk legosh lainnya seperti jaket pria dan wanita. Semuanya disajikan spesial untuk mempermudah siapapun menemukan brand legosh asli original yang mana, siapapun dapat melihat detail harga, deskripsi produk, toko dan alamat penjual setelah mengakses tombol detail pada setiap katalog. Segera, miliki fashion terbaik kamu hari ini dan simpan halaman ini sebagai rujukan karena akan selalu di update setiap harinya.</p>
 </section>
 `;
