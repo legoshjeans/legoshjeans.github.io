9bd841195b747619b871dca4e2d797808709ce76
legoshjeans <cstrendfidi@gmail.com>
2026-02-03 02:19:18 +0700

    Update and rename privacy.js to disclaimer.js
    Signed-off-by: legoshjeans <cstrendfidi@gmail.com>

---
+++ disclaimer.js
@@ -0,0 +1,11 @@
+
+document.getElementById("disclaimer").innerHTML = `
+<section>
+<h3>Disclaimer</h3>
+Situs ini dibuat semata untuk menampilkan katalog legosh original yang merupakan celana pria canvas terbaik, namun tidak termasuk dalam hal-hal berikut ini:
+<ul><li>Legosh Jeans bukan bagian situs jual beli celana pria canvas legosh, tetapi hanya mengulas berdasarkan informasi yang diperoleh dari internet.</li><li>
+Kami bukan bagian dari Admin, penjual, pemilik toko atau grosir legosh pekalongan.</li><li>
+Situs ini merupakan milik personal yang dibuat sebagai dokumentasi brand celana jeans pria berkualitas.</li><li>
+Situs ini tidak terhubung ke agensi manapun, tetapi bila ada orang atau pihak legosh terkait yang melihat dan ingin memasarkan produknya melalui iklan banner link, maka bisa langsung menghubungi email kami dibawah.</li></ul>
+</section>
+`;
